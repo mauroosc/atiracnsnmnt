6893b4d5155635e112942be9c4ba77ccb18fe62a
mauroosc <mauroscapolla99@gmail.com>
2024-09-18 01:05:26 -0300

    atira27

--- run.py
+++ run.py
@@ -1,28 +1,5 @@
 from app import create_app
-import psycopg2
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-class database ():
-    
-    conexion = None
-    db = None
-
-
-    def conectar (self):
-        self.conexion = psycopg2.connect (os.environ.get('psql'))
-        self.db = self.conexion.cursor()
-
-
-    def desconectar (self):
-        self.conexion.commit()
-        self.db.close()
-        self.conexion.close()
-
-database_api = database ()
-
-app = create_app(database_api)
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
